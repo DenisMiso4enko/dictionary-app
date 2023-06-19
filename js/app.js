ce60b69b02a97041f5a286f18c6cfa37942d023f
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-06-19 15:38:31 +0300

    feat: add theme toggle, error message, loader

--- js/app.js
+++ js/app.js
@@ -6,9 +6,35 @@ const audioEl = document.querySelector(".audio");
 const playBtn = document.querySelector(".btn-play");
 const listEl = document.querySelector(".dictionary__list");
 const linkEl = document.querySelector(".link");
-
+const loaderEl = document.querySelector("#loader")
+const messageEl = document.querySelector(".message__title");
+const themeCheckEl = document.querySelector("#theme-checkbox");
 const URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 let searchQuery = "";
+
+
+themeCheckEl.addEventListener("change", (e) => {
+  if (event.currentTarget.checked) {
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+  }
+});
+
+input.addEventListener("input", (e) => {
+  searchQuery = e.target.value;
+});
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && searchQuery.toLocaleLowerCase().trim().length > 0) {
+    getWordInfo(URL, searchQuery);
+  }
+});
+playBtn.addEventListener("click", () => {
+  audioEl.play();
+});
+
 
 function renderDefinitions(item) {
   return `
@@ -21,7 +47,7 @@ function getDefinitions(definitions) {
 }
 
 function renderListItem(item) {
-  const syn = item.synonyms.join(", ");
+  const syn = item.synonyms.join(" ");
   return `
     <div class="dictionary__noun">
         <h4 class="section-title">${item.partOfSpeech}</h4>
@@ -48,37 +74,40 @@ function showData(data) {
     meanings: data[0].meanings,
     link: data[0].sourceUrls[0],
   };
-  console.log(wordData.phonetic);
+  messageEl.textContent = "You're word"
   wordEl.textContent = wordData.word;
   transcriptionEl.textContent = wordData.phonetic;
   audioEl.src = wordData.audio;
   linkEl.innerHTML = wordData.link;
+  linkEl.setAttribute("href", wordData.link)
 
   renderList(wordData.meanings);
 }
 
 async function getWordInfo(url, query) {
   try {
+    loaderEl.style.display = "block"
     const request = await fetch(url + query);
     const data = await request.json();
-    // if (!request.status.ok) {
-    //   templateError(data);
-    // }
+    if (request.status === 404) {
+      Toastify({
+        text: `${data.message}`,
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background:  "linear-gradient(to right, #ff5f6d, #ffc371)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+    }
+    loaderEl.style.display = "none"
     showData(data);
   } catch (error) {
     console.log(error);
   }
 }
-// getWordInfo(URL, "hello");
-
-input.addEventListener("input", (e) => {
-  searchQuery = e.target.value;
-});
-input.addEventListener("keydown", (e) => {
-  if (e.key === "Enter" && searchQuery.toLocaleLowerCase().trim().length > 0) {
-    getWordInfo(URL, searchQuery);
-  }
-});
-playBtn.addEventListener("click", () => {
-  audioEl.play();
-});
